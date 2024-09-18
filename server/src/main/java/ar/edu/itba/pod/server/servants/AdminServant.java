0d721c12afb04a3c55bbc480ef2ac1f93539394b
smedin <smedin@itba.edu.ar>
2024-09-18 18:45:32 -0300

    fix: data types

--- server/src/main/java/ar/edu/itba/pod/server/servants/AdminServant.java
+++ server/src/main/java/ar/edu/itba/pod/server/servants/AdminServant.java
@@ -4,6 +4,7 @@ import ar.edu.itba.pod.grpc.admin.*;
 import ar.edu.itba.pod.server.models.Hospital;
 import ar.edu.itba.pod.server.models.Room;
 import com.google.protobuf.BoolValue;
+import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 
 import java.util.Optional;
@@ -18,7 +19,7 @@ public class AdminServant extends AdminServiceGrpc.AdminServiceImplBase {
     }
 
     @Override
-    public void addRoom(RoomData request, StreamObserver<BoolValue> response) {
+    public void addRoom(Empty request, StreamObserver<BoolValue> response) {
         hospital.addRoom(new Room(roomCounter++));
 
         response.onNext(BoolValue.newBuilder().setValue(true).build());
@@ -27,7 +28,7 @@ public class AdminServant extends AdminServiceGrpc.AdminServiceImplBase {
 
     @Override
     public void addDoctor(DoctorData request, StreamObserver<BoolValue> response) {
-        String doctorName = request.getName();
+        String doctorName = request.getDoctorName().getName();
         int maxLevel = Integer.parseInt(request.getLevel());
 
         if (maxLevel < 1 || maxLevel > 5) { // TODO: move inside Doctor (model) class
@@ -52,7 +53,7 @@ public class AdminServant extends AdminServiceGrpc.AdminServiceImplBase {
 
     @Override
     public void defineAvailability(DoctorAvailability request, StreamObserver<BoolValue> response) {
-        String doctorName = request.getDoctor();
+        String doctorName = request.getDoctorName().getName();
         String availability = request.getAvailability(); // TODO: change to enum
 
         boolean defined = hospital.defineDoctorAvailability(doctorName, availability); // TODO: make defineAvailability inside Hospital syncronized (use writeLock)
@@ -67,7 +68,7 @@ public class AdminServant extends AdminServiceGrpc.AdminServiceImplBase {
 
         String availability = hospital.getDoctorAvailability(doctorName); // TODO: check
 
-        DoctorAvailability toReturn = DoctorAvailability.newBuilder().setDoctor(doctorName).setAvailability(availability).build();
+        DoctorAvailability toReturn = DoctorAvailability.newBuilder().setDoctorName(request).setAvailability(availability).build();
 
         response.onNext(toReturn);
         response.onCompleted();
